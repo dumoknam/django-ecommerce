ef836c9ae0fc60a595acc05333e202f7e8e0acb6
dumok <nsh330@gmail.com>
2017-11-30 02:07:40 +0900

    Add register & login views

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class ContactForm(forms.Form):
@@ -23,6 +24,45 @@ class ContactForm(forms.Form):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        if not 'gmail.com' in email:
+        if 'gmail.com' in email:
+            return email
+        else:
             raise forms.ValidationError('Email has to be gmail.com')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+User = get_user_model()
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password',
+                                widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        queryset = User.objects.filter(username=username)
+        if queryset.exists():
+            raise forms.ValidationError("username is taken")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        queryset = User.objects.filter(email=email)
+        if queryset.exists():
+            raise forms.ValidationError("email is taken")
         return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password != password2:
+            raise forms.ValidationError("password didn't match")
+        return data
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from ecommerce.forms import ContactForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, get_user_model
+from ecommerce.forms import ContactForm, LoginForm, RegisterForm
 
 
 def home_page(request):
@@ -8,6 +8,8 @@ def home_page(request):
         'title': 'Hello World!',
         'content': 'Welcome to the homepage',
     }
+    if request.user.is_authenticated():
+        context.update({'premium_content': 'YEAHHHHH'})
     return render(request, "home_page.html", context)
 
 
@@ -35,29 +37,45 @@ def contact_page(request):
     return render(request, "contact/view.html", context)
 
 
-def home_page_raw(request):
-    html_ = """
-    <!doctype html>
-    <html lang="en">
-        <head>
-            <title>Hello, world!</title>
-            <!-- Required meta tags -->
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    print('User logged in')
+    # print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        # username = request['username']
+        # password = request['password']
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page
+            # context['form'] = LoginForm()
+            return redirect('/login')
+        else:
+            # Return an 'invalid login' error message
+            print('Error')
 
-            <!-- Bootstrap CSS -->
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-        </head>
-        <body>
-            <div class='text-center'>
-                <h1>Hello, world!</h1>
-            </div>
-            <!-- Optional JavaScript -->
-            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-        </body>
-    </html>
-    """
-    return HttpResponse(html_)
+    return render(request, 'auth/login.html', context)
+
+
+User = get_user_model()
+
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, 'auth/register.html', context)
