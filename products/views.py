9e8fe40fb693cd1d5accfca3fcfffc76b913a56d
dumok <nsh330@gmail.com>
2017-12-03 15:33:22 +0900

    Add product List & Detail view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = 'products/list.html'
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, 'products/list.html', context)
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+
+def product_detail_view(request, pk=None):
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'object': instance
+    }
+    return render(request, 'products/detail.html', context)
